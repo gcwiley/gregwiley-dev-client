a3a93cdab244275dc380a772ebdc51b1622b3fdd
Greg Wiley <gcwiley@gmail.com>
2023-10-08 13:15:05 -0400

    additional code added

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -2,15 +2,13 @@ import { Component } from '@angular/core';
 import { VERSION } from '@angular/material/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss'],
-  standalone: true,
+   selector: 'app-footer',
+   templateUrl: './footer.component.html',
+   styleUrls: ['./footer.component.scss'],
+   standalone: true,
 })
 export class FooterComponent {
-  version = VERSION.full;
+   version = VERSION.full;
 
-  year = new Date().getFullYear();
-
-  
+   year = new Date().getFullYear();
 }
